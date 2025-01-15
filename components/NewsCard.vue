0a3e57c2aaab4e4b33f57e399676a99c0d7f9378
DevShaded <devshaded@devshaded.com>
2025-01-15 09:21:14 +0100

    feat: news articles front page

---
+++ components/NewsCard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { Post } from "~/types/Post";
+const config = useRuntimeConfig();
+
+defineProps<{
+  post: Post;
+}>();
+</script>
+
+<template>
+  <nuxt-link prefetch-on="interaction" :to="{ name: 'artikler-slug', params:  { slug: post.slug } }" class="bg-white shadow rounded-lg p-4">
+    <article>
+      <nuxt-img :src="config.public.baseApiUrl + '/' + post.image_url" alt="News 2" class="rounded-lg mb-4" />
+      <h4 class="font-bold text-lg mb-2">{{ post.title }}</h4>
+      <p class="text-sm text-gray-600 mb-4">{{ post.lead.trimEnd().slice(0, 100) }}...</p>
+      <p class="text-xs text-gray-500">{{ post.author.name }} • {{ new Date(post.created_at).toLocaleDateString() }}</p>
+    </article>
+  </nuxt-link>
+</template>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import NewsHeaderSkeleton from "~/components/Skeletons/NewsHeaderSkeleton.vue";
+import NewsCardSkeleton from "~/components/Skeletons/NewsCardSkeleton.vue";
+
+const config = useRuntimeConfig();
+const { status, error, data: posts } = useFetch(config.public.baseApiUrl + '/api/posts', {
+  lazy: true
+});
+</script>
+
+<template>
+  <div class="container mx-auto p-6">
+    <!-- Header Section -->
+    <NewsHeaderSection
+      v-if="status === 'success'"
+      :post="posts.data.posts[0]"
+    />
+    <div v-else-if="error">
+      <div class="text-red-600 bg-red-100 rounded-lg p-4">
+        <p class="text-lg font-medium">Kunne ikke laste inn artikler :( {{ error.status }}</p>
+      </div>
+    </div>
+    <div v-else>
+      <NewsHeaderSkeleton />
+    </div>
+
+    <!-- Latest News Section -->
+    <div class="mb-6">
+      <div class="flex justify-between items-center mb-4">
+        <h3 class="text-xl font-bold">Siste Nytt</h3>
+      </div>
+      <div v-if="status === 'success'" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        <NewsCard v-for="post in posts.data.posts.slice(1, 5)" :key="post.id" :post="post" />
+      </div>
+      <div v-else-if="error">
+        <div class="text-red-600 bg-red-100 rounded-lg p-4">
+          <p class="text-lg font-medium">Kunne ikke laste inn artikler :( {{ error.status }}</p>
+        </div>
+      </div>
+      <div v-else class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div v-for="item in 4" :key="item">
+          <NewsCardSkeleton />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
